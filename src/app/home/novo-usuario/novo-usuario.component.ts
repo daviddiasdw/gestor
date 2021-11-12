b00045e6af4419e0b072f3fcdb33ef51d8148c0f
Gieandes Silva <daviddiasdw@gmail.com>
2021-11-12 16:37:40 -0300

    Cadastro de Usuário

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 
@@ -13,15 +14,16 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private router: Router
     ) { }
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      fullName: ['',[Validators.required, Validators.minLength(5)] ],
-      email: ['', [Validators.required, Validators.email] ],
-      userName: ['', [this.novoUsuarioService.userNameMinusculo], [this.novoUsuarioService.usuarioJaExiste()]],
-      password: ['', [Validators.minLength(8)]]
+        fullName: ['',[Validators.required, Validators.minLength(5)] ],
+        email: ['', [Validators.required, Validators.email] ],
+        userName: ['', [this.novoUsuarioService.userNameMinusculo], [this.novoUsuarioService.usuarioJaExiste()]],
+        password: ['']
     },
     {
       validators: [this.novoUsuarioService.usuarioSenhaValidator],
@@ -30,8 +32,17 @@ export class NovoUsuarioComponent implements OnInit {
   }
 
   cadastrar(){
-    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    console.log(novoUsuario)
+    if (this.novoUsuarioForm.valid) {
+      const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+      this.novoUsuarioService.cadastraNovoUsuario(novoUsuario).subscribe(
+        () => {
+          this.router.navigate([''])
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    }
   }
 
 }
